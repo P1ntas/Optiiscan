69bab1fff020a2708e5b4dea40ef466be037eacd
Pedro Silva <pedroafsuper@gmail.com>
2024-04-05 13:59:48 +0100

    GPT-4V model API setup, (prompt with json output and multiple images)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,19 @@
 </script>
 
 <slot />
+
+<style lang="postcss">
+	:root {
+		background-color: '#ffffff';
+	}
+
+	:global(html) {
+		min-height: 100vh;
+	}
+
+	:global(body) {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1 class="flex text-secondary">Welcome to SvelteKit</h1>
-<button class="btn btn-primary m-auto flex h-auto w-36 p-5">Button</button>
+<button class="btn btn-primary mx-auto flex h-auto w-36 p-5">Button</button>
