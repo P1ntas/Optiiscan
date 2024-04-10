54510db5734faeec9777a14a5581ad7f8657ea1b
Manuel Amorim <manueljoaoamorim@gmail.com>
2024-04-10 19:41:06 +0100

    Create other pages

---
+++ src/routes/records/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Nav from '../../components/Nav.svelte';
+</script>
+
+<div class="flex-column flex">
+	<Nav page="records" />
+
+	<main class="flex-1 p-10"></main>
+</div>
+
+<style>
+</style>
