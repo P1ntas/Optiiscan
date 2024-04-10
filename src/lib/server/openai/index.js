15d55c7c07d5d8e10a28990d18247f3588b73c7b
P1ntas <72892065+P1ntas@users.noreply.github.com>
2024-04-10 18:37:31 +0100

    Working on prompt engineering

--- src/lib/server/openai/index.js
+++ src/lib/server/openai/index.js
@@ -5,14 +5,18 @@ import fs from 'fs-extra';
 const config = {
 	ENDPOINT: 'https://gpt4-vision-feup.openai.azure.com/',
 	DEPLOYMENT_NAME: 'GPT4-vision-feup',
-	SERVER_PROMPT:
-		'You are a helpful assistant designed with analyzing images and yielding their relevant information, \
-	outputting in JSON format, in a single-line without whitespaces. For each image, the JSON must include an object with \
-	the names "title" (a title that fits the image), "description" (short, one sentence description) and "color" (the predominant color in the image).\
-	Make sure the JSON objects are returned inside of a list, even when there is only one image.',
-	USER_PROMPT: "What's the landscape like in this/these image(s)?",
+	SERVER_PROMPT: 'You are a helpful assistant designed with analyzing images and yielding their relevant information, \
+    outputting in JSON format, in a single-line without whitespaces. The images are from the boxes of frozen \
+    products. For each image, the JSON must include an object with \
+    the names "name" (the name of the name), "nutritional table" (has several lines, is structured in a table format). \
+	The nutritional table has  multiple lines and columns, please make sure to include all the information. \
+	Please do  not invent values, use the information that is present in the image. Values vertically aligned are in the same column \
+	The object should also include the ingredients listed in the "ingredients" section of the image. \
+	Please extract icons from the image and derive their meaning as a "icons" attribute in the object. \
+    Make sure the JSON objects are returned inside of a list, even when there is only one image.',
+    USER_PROMPT: "What's the name and nutritional table like in this/these image(s)? You can't say I cannot assist you with the request",
 	TEMPERATURE: 0,
-	MAX_TOKENS: 100
+	MAX_TOKENS: 1000
 };
 const client = new OpenAIClient(config.ENDPOINT, new AzureKeyCredential(OPENAI_API_KEY));
 
