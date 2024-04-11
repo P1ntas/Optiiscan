0a76fea12ac5aeaa79b105a856ee92f05a8b60d8
Manuel Amorim <manueljoaoamorim@gmail.com>
2024-04-11 01:25:08 +0100

    Upload photos area under drop box

--- src/components/Dropzone.svelte
+++ src/components/Dropzone.svelte
@@ -1,45 +1,48 @@
 <script>
 	import { Dropzone } from 'flowbite-svelte';
+	import { Listgroup, ListgroupItem } from 'flowbite-svelte';
 
-	let value = [];
+	let types = ['image/png', 'image/jpeg', 'image/jpg', 'application/pdf', 'application/zip'];
+
+	let files = [];
+	$: {
+		console.log(files);
+	}
+
+	//uploading by drag
 	const dropHandle = (event) => {
-		value = [];
 		event.preventDefault();
 		if (event.dataTransfer.items) {
 			[...event.dataTransfer.items].forEach((item, i) => {
 				if (item.kind === 'file') {
 					const file = item.getAsFile();
-					value.push(file.name);
-					value = value;
+					//Check file type
+					if (types.includes(file.type)) {
+						files.push(file);
+					}
 				}
 			});
+			files = files;
 		} else {
 			[...event.dataTransfer.files].forEach((file, i) => {
-				value = file.name;
+				if (types.includes(file.type)) files.push(file);
 			});
+			files = files;
 		}
 	};
 
+	//Uploading by click
 	const handleChange = (event) => {
-		const files = event.target.files;
-		if (files.length > 0) {
-			value.push(files[0].name);
-			value = value;
+		const new_files = event.target.files;
+
+		if (new_files.length > 0) {
+			for (let i = 0; i < new_files.length; i++) {
+				if (types.includes(new_files[i].type)) {
+					files.push(new_files[i]);
+				}
+			}
+			files = files;
 		}
-	};
-
-	const showFiles = (files) => {
-		if (files.length === 1) return files[0];
-		let concat = '';
-		files.map((file) => {
-			concat += file;
-			concat += ',';
-			concat += ' ';
-		});
-
-		if (concat.length > 40) concat = concat.slice(0, 40);
-		concat += '...';
-		return concat;
 	};
 </script>
 
@@ -50,6 +53,8 @@
 		event.preventDefault();
 	}}
 	on:change={handleChange}
+	multiple
+	accept={types.join(', ')}
 >
 	<svg
 		aria-hidden="true"
@@ -65,12 +70,29 @@
 			d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
 		/></svg
 	>
-	{#if value.length === 0}
-		<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-			<span class="font-semibold">Click to upload</span> or drag and drop
-		</p>
-		<p class="text-xs text-gray-500 dark:text-gray-400">PNG, JPG, PDF or ZIP (Max 10MB)</p>
-	{:else}
-		<p>{showFiles(value)}</p>
-	{/if}
+
+	<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+		<span class="font-semibold">Click to upload</span> or drag and drop
+	</p>
+	<p class="text-xs text-gray-500 dark:text-gray-400">PNG, JPG, PDF or ZIP (Max 10MB)</p>
 </Dropzone>
+
+{#if files.length > 0}
+	<Listgroup class="mt-3">
+		{#each files as file, i}
+			<ListgroupItem key={i} class="border-1 flex">
+				{#if file.type === 'image/png' || file.type === 'image/jpeg' || file.type === 'image/jpg'}
+					<img alt="" class="h-11 w-11" src={URL.createObjectURL(file)} />
+				{:else if file.type === 'application/pdf'}
+					<img alt="" src="" />
+				{:else if file.type === 'application/zip'}
+					<img alt="" src="" />
+				{/if}
+				<div class="ml-2">
+					<p class="text-black">{file.name}</p>
+					<p class="text-gray-500">{Math.floor(file.size / 1000)} kB</p>
+				</div>
+			</ListgroupItem>
+		{/each}
+	</Listgroup>
+{/if}
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -3,13 +3,15 @@
 	import Nav from '../../components/Nav.svelte';
 
 	import { Heading, P, Button } from 'flowbite-svelte';
+
+	let files = [];
 </script>
 
 <div class="flex-column flex">
 	<Nav page="upload" />
 
 	<main class="flex-1 p-10">
-		<Heading tag="h2">Upload Image(s)</Heading>
+		<Heading tag="h2" class="text-black">Upload Image(s)</Heading>
 		<P>Add your products packaging images here</P>
 		<hr class="my-6 h-px border-0 bg-gray-200 dark:bg-gray-700" />
 		<Dropzone />
