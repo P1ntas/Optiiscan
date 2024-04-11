99b65a3dc27c703cce628d751c97be473546a9bd
Manuel Amorim <manueljoaoamorim@gmail.com>
2024-04-11 18:40:37 +0100

    starting the table

--- src/routes/records/+page.svelte
+++ src/routes/records/+page.svelte
@@ -3,6 +3,15 @@
 	import SearchBar from '../../components/SearchBar.svelte';
 	import { Heading, P, Button } from 'flowbite-svelte';
 	import { DownloadSolid } from 'flowbite-svelte-icons';
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		Checkbox
+	} from 'flowbite-svelte';
 
 	let products = [];
 	$: numProducts = products.length;
@@ -50,6 +59,72 @@
 		</div>
 
 		<p>Showing <span class="font-bold">{numProducts} products</span></p>
+
+		<Table hoverable={true} class="mt-10">
+			<TableHead class="text-neutral-600">
+				<TableHeadCell class="!p-4">
+					<Checkbox color="red" />
+				</TableHeadCell>
+				<TableHeadCell class="font-medium">Upload time</TableHeadCell>
+				<TableHeadCell class="font-medium">Code</TableHeadCell>
+				<TableHeadCell class="font-medium">Name</TableHeadCell>
+				<TableHeadCell class="font-medium">Description</TableHeadCell>
+				<TableHeadCell class="font-medium">Labels</TableHeadCell>
+				<TableHeadCell>
+					<span class="sr-only">Edit</span>
+				</TableHeadCell>
+			</TableHead>
+			<TableBody>
+				<TableBodyRow>
+					<TableBodyCell class="!p-4">
+						<Checkbox />
+					</TableBodyCell>
+					<TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
+					<TableBodyCell>Sliver</TableBodyCell>
+					<TableBodyCell>Laptop</TableBodyCell>
+					<TableBodyCell>$2999</TableBodyCell>
+					<TableBodyCell>
+						<a
+							href="/tables"
+							class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+							>Edit</a
+						>
+					</TableBodyCell>
+				</TableBodyRow>
+				<TableBodyRow>
+					<TableBodyCell class="!p-4">
+						<Checkbox />
+					</TableBodyCell>
+					<TableBodyCell>Microsoft Surface Pro</TableBodyCell>
+					<TableBodyCell>White</TableBodyCell>
+					<TableBodyCell>Laptop PC</TableBodyCell>
+					<TableBodyCell>$1999</TableBodyCell>
+					<TableBodyCell>
+						<a
+							href="/tables"
+							class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+							>Edit</a
+						>
+					</TableBodyCell>
+				</TableBodyRow>
+				<TableBodyRow>
+					<TableBodyCell class="!p-4">
+						<Checkbox />
+					</TableBodyCell>
+					<TableBodyCell>Magic Mouse 2</TableBodyCell>
+					<TableBodyCell>Black</TableBodyCell>
+					<TableBodyCell>Accessories</TableBodyCell>
+					<TableBodyCell>$99</TableBodyCell>
+					<TableBodyCell>
+						<a
+							href="/tables"
+							class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+							>Edit</a
+						>
+					</TableBodyCell>
+				</TableBodyRow>
+			</TableBody>
+		</Table>
 	</main>
 </div>
 
