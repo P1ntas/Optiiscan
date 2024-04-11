a6d57d7f2537042ae7c2a966c5a77354152310ff
Manuel Amorim <manueljoaoamorim@gmail.com>
2024-04-11 18:01:31 +0100

    Export csv and heading

--- src/routes/records/+page.svelte
+++ src/routes/records/+page.svelte
@@ -1,6 +1,11 @@
 <script>
 	import Nav from '../../components/Nav.svelte';
 	import SearchBar from '../../components/SearchBar.svelte';
+	import { Heading, P, Button } from 'flowbite-svelte';
+	import { DownloadSolid } from 'flowbite-svelte-icons';
+
+	let products = [];
+	$: numProducts = products.length;
 
 	function search(event) {
 		let searchText = event.detail.searchText;
@@ -9,6 +14,24 @@
 		console.log('Searching for:', searchText, 'in category:', category);
 
 		//TODO: Add search call to backend here
+		//products = ...
+	}
+
+	function downloadCSV() {
+		//TODO: Add here call to backend to download CSV
+
+		const fileUrl = '';
+		const filename = 'output.csv';
+
+		// Create a temporary anchor element
+		const link = document.createElement('a');
+		link.href = fileUrl;
+		link.setAttribute('download', 'output.csv');
+
+		// Programmatically click the anchor element to trigger the download
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
 	}
 </script>
 
@@ -17,6 +40,16 @@
 	<main class="flex-1 p-10">
 		<SearchBar on:search={search} />
 		<hr class="my-7 h-px border-0 bg-black bg-opacity-10" />
+
+		<div class="flex">
+			<Heading tag="h3" class="text-black">Products</Heading>
+			<Button on:click={downloadCSV} color="red" size="md" class="w-40">
+				<DownloadSolid class="me-2 h-4 w-4" />
+				Export CSV
+			</Button>
+		</div>
+
+		<p>Showing <span class="font-bold">{numProducts} products</span></p>
 	</main>
 </div>
 
