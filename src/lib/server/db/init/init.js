8c9947df545e352febe9d32ebdb4a971c9490ecf
alex-bsT <alexandrefn18@gmail.com>
2024-04-28 19:58:54 +0100

    Products REST API + integration of backend/frontend

--- src/lib/server/db/init/init.js
+++ src/lib/server/db/init/init.js
@@ -52,3 +52,51 @@ const logs = Array.from({ length: 25 }, () => {
 });
 
 db.logs.insertMany(logs); // eslint-disable-line
+
+// =========================================================================
+// =========================================================================
+// =========================================================================
+
+db.products.insertMany([
+	// eslint-disable-line
+	{
+		uploadTime: '2023-07-15T10:30',
+		code: '987654321',
+		name: 'Organic Chocolate Bar',
+		description:
+			'Indulge in the rich, creamy taste of our organic chocolate bar. Made from the finest cocoa beans sourced from sustainable farms, each bite offers a luxurious blend of sweetness and depth. Whether you savor it on its own or use it as a decadent addition to your favorite desserts, our chocolate bar is sure to delight your senses and satisfy your cravings.',
+		labels: ['Organic', 'Fair Trade', 'Vegan']
+	},
+	{
+		uploadTime: '2023-09-28T16:45',
+		code: '123456789',
+		name: 'Spicy Jalapeno Chips',
+		description:
+			'Experience a fiery burst of flavor with our spicy jalapeno chips. Crafted from premium potatoes and seasoned with an irresistible blend of spices, each chip delivers a perfect balance of heat and crunch. Whether you enjoy them as a zesty snack on-the-go or pair them with your favorite dip, our jalapeno chips are sure to add a kick to any occasion.',
+		labels: ['Spicy', 'Gluten-Free', 'Non-GMO']
+	},
+	{
+		uploadTime: '2024-02-10 08:15',
+		code: '246810975',
+		name: 'Coconut Bliss Granola',
+		description:
+			'Indulge in a tropical escape with our coconut bliss granola. Packed with toasted coconut flakes, crunchy almonds, and juicy dried fruits, each spoonful transports you to paradise. Whether you enjoy it with yogurt for a wholesome breakfast or sprinkle it over ice cream for a decadent dessert, our granola is a delightful treat any time of day.',
+		labels: ['Gluten-Free', 'Organic', 'Vegan']
+	},
+	{
+		uploadTime: '2024-03-20 14:00',
+		code: '135792468',
+		name: 'Honey Roasted Cashews',
+		description:
+			'Satisfy your cravings with our honey roasted cashews. Each cashew is coated in a golden honey glaze and oven-roasted to perfection, resulting in a sweet and crunchy snack thats impossible to resist. Whether you enjoy them on their own or sprinkle them over salads for added crunch, our cashews are sure to be a hit with everyone.',
+		labels: ['Gluten-Free', 'Non-GMO']
+	},
+	{
+		uploadTime: '2024-04-25 11:30',
+		code: '369258147',
+		name: 'Blueberry Greek Yogurt',
+		description:
+			'Indulge in the creamy goodness of our blueberry Greek yogurt. Made with thick, velvety yogurt and ripe, juicy blueberries, each spoonful is a burst of fruity flavor and probiotic goodness. Whether you enjoy it as a quick breakfast on-the-go or a refreshing snack between meals, our Greek yogurt is a delicious way to fuel your day.',
+		labels: ['Probiotic', 'High Protein']
+	}
+]);
--- src/routes/api/products/+server.js
+++ src/routes/api/products/+server.js
@@ -1,6 +1,82 @@
 import { json } from '@sveltejs/kit';
 import db from '../../../lib/server/db/db';
+import { ObjectId } from 'mongodb';
+
+/**
+ * Products REST API
+ *
+ * --> GET
+ *  Returns the list of products
+ *
+ * --> POST
+ *  Create a new product
+ *
+ * --> PATCH
+ *  Modify a existing product
+ *
+ * --> DELETE
+ *  Deletes an existing product
+ */
 
 export async function GET() {
 	return json(await (await db).collection('products').find().toArray());
+}
+
+export async function POST({ request }) {
+	const data = await request.json();
+	data['uploadTime'] = new Date().toISOString();
+	await (await db).collection('products').insertOne(data);
+	return json({ success: true, message: 'Document inserted successfully.' });
+}
+
+export async function PATCH({ request }) {
+	const data = await request.json();
+	if (!data) {
+		return json({ success: false, message: 'No data provided for update.' }, 400);
+	}
+
+	const id = new ObjectId(data['id']);
+	const existingProduct = await (await db).collection('products').findOne({ _id: id });
+	if (!existingProduct) {
+		return json({ success: false, message: 'Log not found.' }, 404);
+	}
+
+	const updateFields = {
+		code: data['code'],
+		name: data['name'],
+		description: data['description'] ?? '',
+		labels: data['labels'] ?? ''
+	};
+
+	if (Object.keys(updateFields).length === 0) {
+		return json({ success: false, message: 'No valid fields provided for update.' }, 400);
+	}
+
+	const result = await (await db)
+		.collection('products')
+		.updateOne({ _id: id }, { $set: updateFields });
+
+	if (result.modifiedCount === 1) {
+		return json({ success: true, message: 'Log updated successfully.' });
+	} else {
+		return json({ success: false, message: 'Log not found or no updates made.' }, 404);
+	}
+}
+
+export async function DELETE({ request }) {
+	const ids = await request.json();
+	if (!ids) {
+		return json({ success: false, message: 'No product IDs provided for deletion.' }, 400);
+	}
+
+	const idArray = ids.split(',').map((id) => {
+		return new ObjectId(id);
+	});
+	const result = await (await db).collection('products').deleteMany({ _id: { $in: idArray } });
+
+	if (result.deletedCount > 0) {
+		return json({ success: true, message: 'Products deleted successfully.' });
+	} else {
+		return json({ success: false, message: 'Products not found or already deleted.' }, 404);
+	}
 }
